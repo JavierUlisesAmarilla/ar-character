5ebc435668447b1d67c5961330671684c4fd04ac
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2024-02-16 17:35:50 -0300

    audio

--- src/Scene/R3fScene/Player.tsx
+++ src/Scene/R3fScene/Player.tsx
@@ -1,10 +1,19 @@
 // import {useXR} from '@react-three/xr'
+import {useEffect} from 'react'
+import {useAudioPlayer} from 'react-use-audio-player'
 import {Model} from '../Utils/Model'
 
 
 export const Player = () => {
   // const player = useXR((state) => state.player)
   // useFrame(() => void (player.rotation.y += 0.0005))
+  const bgAudio = useAudioPlayer()
+
+  useEffect(() => {
+    bgAudio?.load('audios/song.mp3', {loop: true})
+    bgAudio.play()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <Model
