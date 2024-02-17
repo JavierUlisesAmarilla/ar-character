4ccc081dbf84276743c64692ddceca37ba658ca4
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2024-02-16 21:08:10 -0300

    ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
 import cesium from 'vite-plugin-cesium'
@@ -5,5 +6,5 @@ import glsl from 'vite-plugin-glsl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cesium(), glsl()],
+  plugins: [react(), cesium(), glsl(), basicSsl()],
 })
